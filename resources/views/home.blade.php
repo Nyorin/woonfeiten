4b5765c946c04c015da9cfd2d4b9d25ac637f3e2
dennisslimmers <dennisslimmers@live.nl>
2017-06-25 17:51:28 +0200

    Added bootstrap + vue scripts and created temporary landing page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Woonfeiten</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Patrick+Hand+SC" rel="stylesheet">
+
+        <!-- Import Bootstrap/Vue files -->
+        <link href="{{ URL::to('css/app.css') }}" rel="stylesheet" type="text/css">
+        <script src="{{ URL::to('js/app.js') }}"></script>
+        <style>
+            body {
+                background-color: #2ab27b;
+            }
+
+            .title {
+                margin-top: 300px;
+                font-size: 4em;
+                font-family: 'Patrick Hand SC', cursive;
+                color: white;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container text-center">
+           <h1 class="title"><strong>Woonfeiten</strong></h1>
+        </div>
+    </body>
+</html>
